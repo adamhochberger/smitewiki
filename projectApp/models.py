390f0ce2b7cdae2db2e1f717bb809ada79ebf6e9
adamhochberger <adamhochberger@gmail.com>
2022-06-21 21:09:09 -0400

    Adjust some fields on Item to require a value

--- projectApp/models.py
+++ projectApp/models.py
@@ -4,15 +4,14 @@ from projectApp.utils.constants import ItemTypes
 
 
 class Item:
-    name = models.TextField(default="")
-    stats = models.JSONField(default="")
+    name = models.TextField()
+    stats = models.JSONField()
+    cost = models.PositiveSmallIntegerField()
+
     icon = models.ImageField(default=None)
     type = models.JSONField(default=ItemTypes.NEUTRAL)
-    cost = models.PositiveSmallIntegerField(default=0)
-
     passive = models.TextField(default="")
     is_starter = models.BooleanField(default=False)
-
 
     previous_item = models.OneToOneField('self', on_delete=models.CASCADE)
     next_item = models.ForeignKey('self', on_delete=models.CASCADE)
